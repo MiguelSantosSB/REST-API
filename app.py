c6aa307e8a08b2812d921ef577bbe641e0284fac
Miguel Santos <miguel.santoss085@gmail.com>
2023-03-08 16:21:59 -0300

    flask estudo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,15 +7,52 @@ lojas = [
         "name": "minha loja",
         "itens": [
             {
-                "nome": "arroz",
-                "preco": 5.64,
                 "nome": "feijao",
-                "preco": 6.32,
+                "preco": 6.32
             },
+            {
+               "nome": "arroz",
+                "preco": 5.64 
+            }
         ]
     }
 ]
-
+# Criando o sistema de lojas
 @app.get("/lojas")
 def get_loja():
     return {"lojas": lojas}
+
+# Adicionando uma nova loja ao sistema
+@app.post("/lojas")
+def create_loja():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "itens":[]}
+    lojas.append(new_store)
+    return new_store, 201
+
+# Adicionando os itens da loja no banco
+@app.post("/lojas/<string:name>/itens")
+def create_item(name):
+    request_data = request.get_json()
+    for store in lojas:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "preco": request_data["preco"]}
+            store["itens"].append(new_item)
+            return new_item, 201
+    return {"menssage": "Loja não encontrada"}, 404
+
+# procura se a loja especifica está armazenada e se sim retorna a loja especifica
+@app.get("/lojas/<string:name>")
+def get_store(name):
+    for store in lojas:
+        if store["name"] == name:
+            return store
+    return {"menssage": "Loja não encontrada"}, 404
+
+# procura se uma loja está armazenada e se sim irá retorma seus itens
+@app.get("/lojas/<string:name>/itens")
+def get_item_in_store(name):
+    for store in lojas:
+        if store["name"] == name:
+            return {"itens": store["itens"]}
+    return {"menssage": "Loja não encontrada"}, 404

fd88454344e1756865a4f3b544ea6648605ab948
Miguel Santos <miguel.santoss085@gmail.com>
2023-03-12 18:50:42 -0300

    iniciando com SQLAlchemy

---
+++ models/item.py
@@ -0,0 +1,9 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablbename__="itens"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    preco = db.Column(db.Float(precison=2), unique=False, nullable=False)
+    loja_id = db.Column(db.Integer, unique=False, nullable=False)
